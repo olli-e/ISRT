59e882ed7006d3b17a89efac3a768770366e7055
Olli E <70414281+olli-e@users.noreply.github.com>
2021-12-07 19:20:25 +0100

    update v1.4 final

--- isrt.py
+++ isrt.py
@@ -74,6 +74,14 @@ class dbgui(QtWidgets.QWidget):
                 self, 'Select Database', db_select_directory, '*.db',)
             self.dbgui.label_dbi_selected_db.setText(self.dbi_path[0])
         elif db_action == 'replace_db':
+            server.create_db_backup()
+            msg13 = QtWidgets.QMessageBox()
+            msg13.setWindowIcon(QtGui.QIcon(":/img/img/isrt.ico"))
+            msg13.setIcon(QtWidgets.QMessageBox.Information)
+            msg13.setWindowTitle("ISRT DB Backup created")
+            msg13.setText(
+                "I created a backup of your existing \nRestarting ISRT!")
+            msg13.exec_()
             if self.dbi_path and self.dbi_path[0].endswith(".db"):
                 # Database connection setup for Importing
                 dbimportdir = self.dbi_path[0]
